f25198c9da199d38a85dc0ef183bd2fc5b3b1007
adwd <ecdt.mn@gmail.com>
2018-06-28 16:23:14 +0900

    add graphql

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,33 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "http://api.githunt.com/graphql"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        feed(type: HOT, limit: 5) {
+          repository {
+            owner {
+              login
+            }
+            name
+          }
+          postedBy {
+            login
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
