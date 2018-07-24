f4cffbe3866b5df00af479b892c835a4825bd813
adwd <ecdt.mn@gmail.com>
2018-07-24 17:21:29 +0900

    use require

--- src/__generated__/fragmentTypes.ts
+++ src/__generated__/fragmentTypes.ts
@@ -1,16 +1,5 @@
 import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
-
-const json = {
-  __schema: {
-    types: [
-      {
-        kind: 'INTERFACE',
-        name: 'Node',
-        possibleTypes: [{ name: 'Todo' }, { name: 'User' }],
-      },
-    ],
-  },
-};
+const json = require('./fragmentTypes.json');
 
 export const fragmentMatcher = new IntrospectionFragmentMatcher({
   introspectionQueryResultData: json,
